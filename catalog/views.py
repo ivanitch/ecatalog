13cf1e4c0cd2f7f3732501487e4a7818a971bb04
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-06-01 21:53:26 +0900

    feat(pages): add home and contacts pages

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,9 @@
+from  django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
-# Create your views here.
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, 'catalog/index.html')
+
+
+def contacts(request: HttpRequest) -> HttpResponse:
+    return render(request, 'catalog/contacts.html')

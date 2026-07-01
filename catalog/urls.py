f6e82d700899e2f157308f19afd409a4f30ce8bb
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-07-01 10:17:42 +0900

    feat(category): add category crud

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path
 
 from . import views
+from .views import (
+    CategoryListView,
+    CategoryDetailView,
+    CategoryCreateView,
+    CategoryUpdateView,
+    CategoryDeleteView
+)
 
 app_name = "catalog"
 
@@ -9,4 +16,11 @@ urlpatterns = [
     path("contacts/", views.contacts, name="contacts"),
     path("products/<int:pk>/", views.product_detail, name="product_detail"),
     path("products/create/", views.product_create, name="product_create"),
+
+    # Category CRUD
+    path("categories/", CategoryListView.as_view(), name="category_list"),
+    path("category/create/", CategoryCreateView.as_view(), name="category_create"),
+    path("category/<int:pk>/", CategoryDetailView.as_view(), name="category_detail"),
+    path("categories/<int:pk>/edit/", CategoryUpdateView.as_view(), name="category_edit"),
+    path("categories/<int:pk>/delete/", CategoryDeleteView.as_view(), name="category_delete"),
 ]

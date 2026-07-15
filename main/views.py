1c0d426f610128e4a6ca4e9451ff3e2363f8c1ef
Sergey Ivanov <ivanitch.mail@gmail.com>
2026-07-15 10:58:24 +0900

    feat(form): add contact form

--- main/views.py
+++ main/views.py
@@ -1,15 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
+from django.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+from django.urls import reverse_lazy
 
-class ContactsView(View):
+from .forms import ContactForm
+
+
+class ContactsView(SuccessMessageMixin, View):
     """Страница контактов"""
 
     @staticmethod
-    def get(request, *args, **kwargs):
-        return render(request, "contacts.html")
+    def get(request):
+        form = ContactForm()
+        return render(request, "contacts.html", {"form": form})
 
     @staticmethod
-    def post(request, *args, **kwargs):
-        name = request.POST.get("name")
-        messages.success(request, f"Спасибо, {name}! Мы свяжемся с вами.")
-        return redirect("catalog:contacts")
+    def post(request):
+        form = ContactForm(request.POST)
+
+        if form.is_valid():
+            # Данные формы валидны
+            name = form.cleaned_data.get('name')
+            phone = form.cleaned_data.get('phone')
+            message = form.cleaned_data.get('message')
+
+            # Здесь можно отправить письмо, сохранить в БД и т.д.
+            # Например:
+            # send_email(name, phone, message)
+
+            success_message = "Спасибо, %(name)s! Мы свяжемся с вами." if phone else "Спасибо, %(name)s!"
+            success_url = reverse_lazy("main:contacts")
+
+            messages.success(request, success_message % {'name': name})
+
+            return redirect(success_url)
+        else:
+            # Если форма невалидна, показываем ошибки
+            return render(request, "contacts.html", {'form': form})
